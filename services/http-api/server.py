eaf30c9a4adbec3392c621d9abaf9946691bd64f
dmimar382 <dmimar382@gmail.com>
2024-10-08 11:50:30 -0500

    implmented user input validation and improved error handling

--- services/http-api/server.py
+++ services/http-api/server.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import RedirectResponse
-from pydantic import BaseModel
+from pydantic import BaseModel, AnyHttpUrl
 import redis.asyncio as redis
 from motor.motor_asyncio import AsyncIOMotorClient
 import logging  
@@ -26,29 +26,35 @@ url_collection = db.urls
 url_shortener = URLShortener(mongo_collection=url_collection, redis_client=redis_client)
 
 class ShortenRequest(BaseModel):
-    url: str
+    url: AnyHttpUrl # Automatic url validation 
     expiration: int = None
 
 
 @app.get("/r/{short_url}")
 async def resolve_url(short_url: str):
-    original_url = await url_shortener.resolve_url(short_url)
-    if original_url:
-        return RedirectResponse(url=original_url)
-    else:
-        raise HTTPException(status_code=404, detail="Short URL not found")
+    try:
+        original_url = await url_shortener.resolve_url(short_url)
+        if original_url:
+            return RedirectResponse(url=original_url)
+        else:
+            raise HTTPException(status_code=404, detail="Short URL not found")
+    except Exception as e:
+        if isinstance(e, HTTPException):
+            raise e
+        else:
+            raise HTTPException(status_code=500, detail=f"An unexpected error occurred: {str(e)}")
 
 
 
 @app.post("/url/shorten")
 async def shorten_url(request: ShortenRequest):
     try:
-        short_url = await url_shortener.shorten_url(request.url, request.expiration)
+        short_url = await url_shortener.shorten_url(str(request.url), request.expiration)
         full_shortened_url = f"{settings.base_url}/r/{short_url}"
         return {"short_url": full_shortened_url}
     except Exception as e:
         logging.error(f"Error shortening URL: {str(e)}")
-        raise HTTPException(status_code=500, detail="Failed to shorten the URL")
+        raise HTTPException(status_code=500, detail=f"Failed to shorten the URL, {str(e)}")
 
 
 @app.get("/")
